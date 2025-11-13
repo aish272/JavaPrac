4cead8dd89b53737956f72381934cc0ae7f5e581
Aishwarya Mishra <mishra.aishwarya97@gmail.com>
2025-11-14 01:43:48 +0530

    Binary search: searchIn2DMatrix

---
+++ src/main/java/com/kadaneAndBS/Q_SearchIn2DMatrix.java
@@ -0,0 +1,43 @@
+package main.java.com.kadaneAndBS;
+
+public class Q_SearchIn2DMatrix {
+
+    /**
+     * <a href="https://leetcode.com/problems/search-a-2d-matrix/description">74. Search a 2D Matrix</a>
+     */
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+
+        int high = (matrix.length * matrix[0].length)-1;
+        int low = 0;
+        while(high>=low)
+        {
+            int mid = (high+low)/2;
+            int row = mid/matrix[0].length;
+            int col = mid%matrix[0].length ;
+
+            if(matrix[row][col]==target)
+            {
+                return true;
+            }
+            else if(matrix[row][col]<target)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+
+        }
+        return false;
+
+    }
+
+    public static void main(String [] args)
+    {
+        int[][] arr = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        System.out.println(searchMatrix(arr,3));
+
+    }
+}
