6e21bb95ca55011f73e78921f44c9d39575b418b
Aishwarya Mishra <mishra.aishwarya97@gmail.com>
2025-12-31 02:43:41 +0530

    I_NumOfSubArrOfSizeK_AvgGreaterOrEqualsThreshold

---
+++ src/main/java/com/twoPointerAndSlidingWindow/I_NumOfSubArrOfSizeK_AvgGreaterOrEqualsThreshold.java
@@ -0,0 +1,34 @@
+package main.java.com.twoPointerAndSlidingWindow;
+
+public class I_NumOfSubArrOfSizeK_AvgGreaterOrEqualsThreshold {
+
+    /**
+     * <a href="https://leetcode.com/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold">1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold</a>
+     */
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+
+        int left = 0;
+        int right = k-1;
+        int ans = 0;
+        while(right<arr.length)
+        {
+            int sum = 0;
+            int count = 0;
+            while(count<k)
+            {
+                sum += arr[left+count];
+                count++;
+            }
+            if((sum/k)>=threshold)
+            {
+                ans ++;
+            }
+            left++;
+            right++;
+        }
+        return ans;
+
+    }
+
+
+}
