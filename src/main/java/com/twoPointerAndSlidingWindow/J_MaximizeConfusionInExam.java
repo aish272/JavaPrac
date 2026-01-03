8b9d50478cbccb68c9ff3f03b67865228bc63f30
Aishwarya Mishra <mishra.aishwarya97@gmail.com>
2026-01-04 00:28:02 +0530

    better of Maximize the Confusion of an Exam

--- src/main/java/com/twoPointerAndSlidingWindow/J_MaximizeConfusionInExam.java
+++ src/main/java/com/twoPointerAndSlidingWindow/J_MaximizeConfusionInExam.java
@@ -54,4 +54,40 @@ public class J_MaximizeConfusionInExam {
         return max;
 
     }
+
+    /**
+     * <a href="https://www.youtube.com/watch?v=vY06L8hZVGI&list=PLpIkg8OmuX-J2Ivo9YdY7bRDstPPTVGvN&index=11">Video solution link</a>
+     */
+    public int maxConsecutiveAnswersBetter(String answerKey, int k) {
+        return Math.max(returnMaxConsecutiveAnswers(answerKey, k, 'T'),
+                returnMaxConsecutiveAnswers(answerKey, k, 'F'));
+
+    }
+
+    private int returnMaxConsecutiveAnswers(String answerKey, int k, char c) {
+        int left = 0;
+        int count = 0;
+        int len = Integer.MIN_VALUE;
+        for (int right = 0; right < answerKey.length();) {
+            char ans = answerKey.charAt(right);
+            if (ans != c) {
+
+                if (count < k) {
+
+                    count++;
+                } else {
+                    while (answerKey.charAt(left) == c) {
+                        left = left + 1;
+                    }
+                    left += 1;
+
+                }
+
+            }
+            len = Math.max(len, right - left + 1);
+            right++;
+
+        }
+        return len;
+    }
 }
