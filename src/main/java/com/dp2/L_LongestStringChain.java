fa1bc3909c72eb0b23662ea2de3600e07dcc223a
Aishwarya Mishra <mishra.aishwarya97@gmail.com>
2026-06-11 21:06:59 +0530

    11th june

--- src/main/java/com/dp2/L_LongestStringChain.java
+++ src/main/java/com/dp2/L_LongestStringChain.java
@@ -43,6 +43,26 @@ public class L_LongestStringChain {
         return Math.max(pick, notPick);
     }
 
+    public int longestStrChainTab(String[] words) {
+
+        int[] dp = new int[words.length];
+        int maxLen = Integer.MIN_VALUE;
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (isPredecessor(words[j], words[i])) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    maxLen = Math.max(maxLen, dp[i]);
+                }
+            }
+        }
+
+        return maxLen == Integer.MIN_VALUE ? 1 : maxLen;
+
+    }
+
     public static boolean isPredecessor(String s1, String s2) {
         int s1Len = s1.length() - 1;
         int s2Len = s2.length() - 1;
